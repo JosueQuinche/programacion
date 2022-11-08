9ef7059d2d1d3153c7dc1d02cdfd56de9baba703
unknown <quinchej659@gmail.com>
2022-11-08 10:23:51 -0600

    chiquitrosfs

---
+++ src/Local.java
@@ -0,0 +1,7 @@
+public class Local {
+    public static void main(String[] args) {
+        System.out.println("Generar un prgrama para encontrar zoom");
+        System.out.println("Encontrar apliacion ");
+
+    }
+}
